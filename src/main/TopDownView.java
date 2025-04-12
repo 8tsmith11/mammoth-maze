c20a3098ab208905f668f6517bb8818387ea1060
bai-allen <iasbnilla@gmail.com>
2025-04-12 00:31:34 -0400

    Update TopDownView.java

--- src/main/TopDownView.java
+++ src/main/TopDownView.java
@@ -1,10 +1,31 @@
 package main;
 
+import java.awt.*;
+
 public class TopDownView {
-	private int[][] world;
-	
-	public TopDownView(int[][] world) {
-		this.world = world;
+	private int[][] map;
+
+	public TopDownView(World world) {
+		this.map = world.getMap();
+	}
+
+	/* draws view at (x, y) on the main view */
+	public void drawView(Graphics g, int x, int y) {
+		Graphics2D g2 = (Graphics2D) g.create();
+		g2.translate(x, y);
+
+		// screen dimensions
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int) screenSize.getWidth();
+		int height = (int) screenSize.getHeight();
+		int cellSize = (width / 2 > height) ? width : height;
+		
+		for (int row = 0; row < this.map.length; row++ ) {
+			for (int col = 0; col < this.map[row].length; col++ ) {
+				g2.drawRect(row, col, cellSize * row, cellSize * col);
+			}
+		}
+		g2.dispose();
 	}
 	
 }
