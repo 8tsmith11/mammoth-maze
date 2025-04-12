7067a760d4252349ba6e432bb92e054d317e6a30
Trevor Smith <91441655+8tsmith11@users.noreply.github.com>
2025-04-12 11:06:34 -0400

    Added grid to TopDownView

--- src/main/TopDownView.java
+++ src/main/TopDownView.java
@@ -22,14 +22,21 @@ public class TopDownView extends Canvas {
 		Graphics g2 = g.create();
 		g2.translate(this.x, this.y);
 		
+		g2.setColor(Color.BLACK);
+		g2.fillRect(x, y, width, height);
+		
 		// set cellSize using width or height of screen depending on which is larger
 		int cellSize = ((this.width < this.height) ? this.width : this.height) / this.map.length;
 		
 		for (int row = 0; row < this.map.length; row++) {
 			for (int col = 0; col < this.map[row].length; col++) {
-				g2.setColor((this.map[row][col] == 0) ? Color.yellow : 
-					Color.black);
+				g2.setColor((this.map[row][col] == 0) ? Color.BLACK : 
+					Color.RED);
 				g2.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
+				
+				// Border around each cell
+				g2.setColor(Color.gray);
+	            g2.drawRect(col * cellSize, row * cellSize, cellSize, cellSize);
 			}
 		}
         
